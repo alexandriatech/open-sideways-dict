fd83cbb7e8d651eaf80fbc53ca4c1b6784c3741a
josue rojas <josuerojas.rojas@gmail.com>
2020-07-15 15:41:03 -0500

    basic login page, animation basics started

--- client/src/Routes/Login/index.jsx
+++ client/src/Routes/Login/index.jsx
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import classNames from "classnames";
+import styles from "./styles.module.scss";
 import GoogleLoginButton from "components-app/GoogleLoginButton";
 import LogOutButton from "components-app/LogOutButton";
 import { UserContext } from "components-app/UserContextProvider";
@@ -9,15 +11,17 @@ const HEADING_TEXT = "Please Login";
 const SUBHEADING_TEXT = "and explain the Javascript World!";
 const SIGNIN_TEXT = "Sign in with Google";
 
-const Login = () => {
+const Login = ({ state, className }) => {
   const { user } = useContext(UserContext);
+  const _className = classNames(className, styles.loginPage, styles[state]);
 
+  console.log("login state", state);
   if (!!user)
     return (
       <LogOutButton render={(props) => <Button {...props}>Logout</Button>} />
     );
   return (
-    <div>
+    <div className={_className}>
       <Heading>{HEADING_TEXT}</Heading>
       <p>{SUBHEADING_TEXT}</p>
       <GoogleLoginButton
--- client/src/Routes/index.jsx
+++ client/src/Routes/index.jsx
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useContext, useRef } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 import { UserContext } from "components-app/UserContextProvider";
 // josue: might use later so leaving it here
 // import AuthenticatedRoute from "components-app/AuthenticatedRoute";
@@ -15,63 +16,85 @@ const ROUTES = (isAuthenticated) => [
   {
     exact: true,
     path: "/",
-    children: <Home />,
+    Component: Home,
     title: "Home",
   },
   {
     exact: true,
     path: "/word/:id",
-    children: "word : id",
+    Component: () => <p>word : id</p>,
     title: "Word Id",
   },
   {
     exact: true,
     path: "/tag/:tag",
-    children: "tag : tag",
+    Component: () => <p>tag : tag</p>,
     title: "Tag",
   },
   {
     exact: true,
     path: "/share/:wordId",
-    children: "Share Id",
+    Component: () => <p>Share Id</p>,
     title: "Share",
   },
   {
     exact: true,
     path: "/user/:id",
-    children: <UserWords />,
+    Component: UserWords,
     title: "User",
   },
   {
     exact: true,
     path: "/about",
-    children: "about",
+    Component: () => <p>about</p>,
     title: "About",
   },
   {
     exact: true,
     path: "/login",
-    children: <Login />,
+    Component: Login,
     title: "Login",
+  },
+  {
+    exact: true,
+    path: "/*",
+    Component: NoMatch,
+    title: "404",
   },
 ];
 
 const Routes = () => {
   const { user } = useContext(UserContext);
+  const location = useLocation();
   const routes = ROUTES(true);
+  const nodeRef = useRef(null);
 
   return (
-    <Router>
+    <>
       <Navbar />
-      <Switch>
-        {routes.map(({ ...routeProps }, i) => (
-          <Route {...routeProps} key={`route-${i}`}></Route>
-        ))}
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+      <SwitchTransition>
+        {/* josue: page transitions should only be scoped for each page 
+        because we are using css modules. although the transitions are similar the components may not be
+        (plus css moduldes) */}
+        <CSSTransition
+          key={location.key}
+          timeout={300}
+          classNames="page"
+          nodeRef={nodeRef}
+          appear
+        >
+          <Switch location={location}>
+            {routes.map(({ Component, ...routeProps }, i) => (
+              <Route key={routeProps.path} {...routeProps}>
+                <div ref={nodeRef}>
+                  <Component />
+                </div>
+              </Route>
+            ))}
+          </Switch>
+        </CSSTransition>
+      </SwitchTransition>
+    </>
   );
 };
 
