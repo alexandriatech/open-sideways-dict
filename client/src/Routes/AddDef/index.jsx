6215653c5e2d10d9f3e059a1329025bf3aa86d79
josue rojas <josuerojas.rojas@gmail.com>
2020-08-12 22:39:59 -0500

    adddef basic form created

--- client/src/Routes/AddDef/index.jsx
+++ client/src/Routes/AddDef/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import { useParams } from "react-router-dom";
+import Button from "components-shared/Button";
+import Heading from "components-shared/Heading";
+import Input from "components-shared/Input";
+import InputTag from "components-shared/InputTag";
 
 const GET_WORD_DATA = gql`
   query WORD($word: String!) {
@@ -16,6 +20,12 @@ const GET_WORD_DATA = gql`
 
 const AddDef = ({ className }) => {
   const { word } = useParams();
+  const [defValue, setDefValue] = useState({
+    value: "",
+    hasError: false,
+    dirty: false,
+  });
+  const [tagValue, setTagValue] = useState([]);
   const { loading, error, data } = useQuery(GET_WORD_DATA, {
     variables: { word },
   });
@@ -24,18 +34,47 @@ const AddDef = ({ className }) => {
   //   TODO: implement
   if (loading) return null;
   if (error) return `Error! ${error}`;
-  let noWordsText;
+  // let noWordsText;
+  let renderMain;
 
   const { getWordByWord } = data;
 
-  if (!getWordByWord) noWordsText = `${word} not found!`;
+  if (!getWordByWord) renderMain = <p>{word} not found!</p>;
+  else {
+    renderMain = (
+      <>
+        <Heading> Add Def for {word}</Heading>
+        <form>
+          <Input className={styles.input} name="Word" value={word} readOnly />
+          <Input
+            className={styles.input}
+            name="Definition"
+            value={defValue.value}
+            onChange={(e) => {
+              const value = e.target.value;
+              setDefValue({
+                value: value,
+                hasError: !value.length,
+                dirty: true,
+              });
+            }}
+          />
+          <InputTag
+            className={styles.input}
+            name={"Tags (max 5)"}
+            tags={tagValue}
+            onChange={setTagValue}
+            maxTags={5}
+          />
+          <Button type={"submit"} appearance="alt">
+            Submit
+          </Button>
+        </form>
+      </>
+    );
+  }
 
-  return (
-    <div className={_className}>
-      Add Def for {word}
-      <p>{noWordsText}</p>
-    </div>
-  );
+  return <div className={_className}>{renderMain}</div>;
 };
 
 AddDef.propTypes = {
---
+++ client/src/components-shared/InputTag/index.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useRef } from "react";
+import styles from "./styles.module.scss";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import xIcon from "assets/x.svg";
+import Icon from "components-shared/Icon";
+
+const Tag = ({ onClick, tag, index }) => (
+  <div className={styles.tag}>
+    {tag}
+    <Icon
+      src={xIcon}
+      width={"16px"}
+      className={styles.xClose}
+      onClick={() => onClick({ tag, index })}
+    />
+  </div>
+);
+
+const InputTag = ({
+  className,
+  maxTags,
+  name,
+  onChange,
+  tags,
+  ...otherProps
+}) => {
+  const _className = classNames(className, styles.input);
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+
+  const removeTag = ({ index }) => {
+    const _tags = [...tags];
+    _tags.splice(index, 1);
+    onChange(_tags);
+  };
+
+  const addTag = () => {
+    const _inputValue = inputValue.trim();
+    if (!tags.includes(_inputValue)) {
+      const _tags = [...tags];
+      _tags.push(_inputValue);
+      onChange(_tags);
+    }
+    setInputValue("");
+  };
+
+  return (
+    <label className={_className} htmlFor={name}>
+      {name}
+      <div className={styles.inputTag} onClick={() => inputRef.current.focus()}>
+        {tags.map((tag, i) => (
+          <Tag key={tag} index={i} tag={tag} onClick={removeTag} />
+        ))}
+        <input
+          className={classNames({ [styles.hide]: tags.length >= maxTags })}
+          ref={inputRef}
+          value={inputValue}
+          onChange={(e) =>
+            !(tags.length >= maxTags) && setInputValue(e.target.value)
+          }
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              if (!(tags.length >= maxTags)) addTag();
+            } else if (
+              (e.keyCode === 46 || e.keyCode === 8) &&
+              !inputValue.length
+            )
+              removeTag({ index: tags.length - 1 });
+          }}
+        ></input>
+      </div>
+    </label>
+  );
+};
+
+InputTag.propTypes = {
+  className: PropTypes.string,
+  maxTags: PropTypes.number,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  tags: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default InputTag;
