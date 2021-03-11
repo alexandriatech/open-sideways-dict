0a051467574b657c6bea173f651a890519f72b06
josuerojasrojas <josuerojas.rojas@gmail.com>
2021-03-11 17:51:32 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const server = require("./graphql");
 const port = process.env.PORT || 4000;
 const path = require("path");
