762c05381c7ffbcb3cc7e78965aef70dd1a05f4c
josuerojasrojas <josuerojas.rojas@gmail.com>
2021-03-11 17:41:19 -0500

    init of seperate repos; added cors to server

--- server/index.js
+++ server/index.js
@@ -5,8 +5,23 @@ const port = process.env.PORT || 4000;
 const path = require("path");
 const publicFolder = "../build";
 
+const WHITELIST_URLS = [process.env.FRONTEND_URL || 'http://example.coms']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (WHITELIST_URLS.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express();
+
+app.use(cors(corsOptions))
+
 server.applyMiddleware({ app });
+
 app.use(express.static(path.join(__dirname, publicFolder)));
 
 app.get("/*", (_, res) => {
