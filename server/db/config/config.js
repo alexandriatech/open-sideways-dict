4ae096c2093c55129ded138ccc9999d3c249ae9c
josuerojasrojas <josuerojas.rojas@gmail.com>
2021-03-16 17:01:19 -0400

    ssl require

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -10,5 +10,11 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 };
